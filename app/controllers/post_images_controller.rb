a30c56ef5fb4cf46c60163a0d89930a84451c3fe
sayabe22 <sayabe22@gmail.com>
2024-07-27 08:39:52 +0000

    [Finish]4-18

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -17,6 +17,7 @@ class PostImagesController < ApplicationController
 
   def show
     @post_image =PostImage.find(params[:id])
+    @post_comment = PostComment.new
   end
 
 
@@ -27,11 +28,11 @@ class PostImagesController < ApplicationController
   end
   
   
-private
+  private
 
-def post_image_params
-  params.require(:post_image).permit(:shop_name, :image, :caption)
-end
+  def post_image_params
+    params.require(:post_image).permit(:shop_name, :image, :caption)
+  end
 
 end
 
