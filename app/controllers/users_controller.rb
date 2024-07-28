2d5d9c78e00e7bd7a2fb15d17ffdd5109cec12ff
sayabe22 <sayabe22@gmail.com>
2024-07-28 13:32:34 +0000

    [finish]4-20

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
-   def update
+  def update
     @user = User.find(params[:id])
     @user.update(user_params)
     redirect_to user_path(@user.id)
