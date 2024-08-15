c0fc45a0b4e8fe2cf9f0f3b5329af5879399501b
sayabe22 <sayabe22@gmail.com>
2024-08-15 13:55:04 +0000

    [Add]認証機能の追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,16 @@
 class UsersController < ApplicationController
+  
   def show
     @user = User.find(params[:id])
     @post_images = @user.post_images.page(params[:page])
   end
 
   def edit
+    is_matching_login_user
     @user = User.find(params[:id])
   end
   def update
+    is_matching_login_user
     @user = User.find(params[:id])
     @user.update(user_params)
     redirect_to user_path(@user.id)
@@ -19,4 +22,12 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :profile_image)
   end
+  
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to post_images_path
+    end
+  end
+  
 end
